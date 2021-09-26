86e3c28e32a3043b1500791d933a503a2439c4a1
Tony Ye <tonyyye@gmail.com>
2021-09-26 00:49:30 -0400

    Add custom domain allowed host

--- clocktower_scripts/settings.py
+++ clocktower_scripts/settings.py
@@ -26,8 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0', '.herokuapp.com']
-
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0', '.herokuapp.com', 'www.clocktowerscripts.com']
 
 # Application definition
 

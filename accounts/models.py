5c130b166c5dcc58a9a9b22728ed82c31fb7072e
Tony Ye <tonyyye@gmail.com>
2021-09-26 03:20:49 -0400

    Update reset email sender field

--- accounts/models.py
+++ accounts/models.py
@@ -77,7 +77,7 @@ def password_reset_token_created(sender, instance, reset_password_token, *args, 
 			# message:
 			email_plaintext_message,
 			# from:
-			'Clocktower Scripts',
+			'noreply@clocktowerscripts.com',
 			# to:
 			[reset_password_token.user.email]
 	)
